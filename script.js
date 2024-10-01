cb146c5f95a1b3d6600769e29eba5cb7a0497cc3
sidman021 <siddusidhartha931@gmail.com>
2024-10-01 22:49:03 +0530

    SliderWrapper is completed

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("JS Connected")
+
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+
+menuItems.forEach((item,index)=>{
+    item.addEventListener("click",()=>{
+        wrapper.style.transform =`translateX(${-100 * index}vw)`
+    })
+})
