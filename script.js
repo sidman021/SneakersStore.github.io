1bffcc63596c386851fcf6f4cdbf16b1b12c8fde
sidman021 <siddusidhartha931@gmail.com>
2024-10-01 23:35:54 +0530

    checkout page is added

--- script.js
+++ script.js
@@ -125,3 +125,15 @@ currentProductColors.forEach((color, index) => {
       size.style.color = "white";
     });
   });
+
+  const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
